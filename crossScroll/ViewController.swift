e394850d639aacde609bc8ec73a3d5576d06f56e
mfouda214 <m.fouda@robone.net>
2018-02-12 22:44:14 +0000

    Developement On

--- crossScroll/ViewController.swift
+++ crossScroll/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  crossScroll
-//
+//  Development Mode On
 //  Created by Mohamed Sobhi  Fouda on 2/12/18.
 //  Copyright © 2018 Mohamed Sobhi Fouda. All rights reserved.
 //
